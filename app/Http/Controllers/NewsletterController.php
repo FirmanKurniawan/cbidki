fb5523a45e1708bc5e39d7d86f567753ff063293
Firman Kurniawan <mrroz3r@gmail.com>
2019-01-22 15:37:03 +0700

    Commit 2

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class NewsletterController extends Controller
 {
-    //
+    public function index()
+   {
+   	return view('admin.newsletter.index');
+   }
+
+   public function delete($id)
+   {
+      $newsletter = Newsletter::find($id);
+      $newsletter->delete();
+      return redirect(url('admin/newsletter'));
+   }
+
+   public function save(Request $r)
+   {
+      $email = $r->email;
+      $newsletter = new Newsletter;
+      $newsletter->email = $email;
+      $newsletter->save();
+
+      return redirect()->back();
+   }
 }
