3e88a7d89d1f04d5b09ec3e7b74e85504847fe5c
farhan <arramsy0902@gmail.com>
2019-01-24 16:52:35 +0700

    Footer

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -113,48 +113,48 @@
 			<footer class="footer-area section-gap">
 				<div class="container">
 					<div class="row">
-						<div class="col-lg-3  col-md-6 col-sm-6">
+						<div class="col-lg-5  col-md-6 col-sm-6">
 							<div class="single-footer-widget">
+								<?php
+								$data = \App\Footer::where('id',1)->first();
+								?>
 								<h4>About Us</h4>
 								<p>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
+									<h3 style="color: black;">{{$data->pengantar1}}</h3>
+									<br>
+									<p style="color: black;">{{$data->pengantar2}}
+									<br>
+									{{$data->pengantar3}}</p>
 								</p>
 							</div>
-						</div>
-						<div class="col-lg-4  col-md-6 col-sm-6">
-							<div class="single-footer-widget">
-								<h4>Contact Us</h4>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
-								</p>
-								<p class="number">
-									012-6532-568-9746 <br>
-									012-6532-569-9748
-								</p>
-							</div>
-						</div>						
+						</div>		
 						<div class="col-lg-5  col-md-6 col-sm-6">
 							<div class="single-footer-widget">
 								<h4>Newsletter</h4>
 								<p>You can trust us. we only send  offers, not a single spam.</p>
-								<div class="d-flex flex-row" id="mc_embed_signup">
-
-
-									  <form class="navbar-form" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get">
+								<div class="d-flex flex-row">
+									  <form class="navbar-form" action="{{url('/admin/newsletter/save')}}" method="post">
+									  	@csrf
 									    <div class="input-group add-on align-items-center d-flex">
-									      	<input class="form-control" name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'" required="" type="email">
-											<div style="position: absolute; left: -5000px;">
-												<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-											</div>
-									      <div class="input-group-btn">
-									        <button class="genric-btn"><span class="lnr lnr-arrow-right"></span></button>
-									      </div>
-									    </div>
-									      <div class="info mt-20"></div>									    
+									      	<input class="form-control" name="email" placeholder="Email address" required="" type="email">
+									      		<div class="input-group-btn">
+									        	<button class="genric-btn"><span class="lnr lnr-arrow-right"></span></button>
+									      		</div>
+									    	</div>									    
 									  </form>
 								</div>
 							</div>
-						</div>						
+						</div>
+						<div class="col-lg-2 col-md-6 col-sm-6 social-widget">
+								<h4>Follow Us</h4>
+								<p>Let us be social</p>
+								<div class="footer-social d-flex align-items-center">
+									<a href="#" target="_blank"><i class="fa fa-facebook" style="padding: 35%; color: black;"></i></a>
+									<a href="#" target="_blank"><i class="fa fa-twitter" style="padding: 35%; color: black;"></i></a>
+									<a href="#" target="_blank"><i class="fa fa-chrome" style="padding: 35%; color: black;"></i></a>
+									<a href="#" target="_blank"><i class="fa fa-instagram" style="padding: 35%; color: black;"></i></a>
+								</div>
+						</div>										
 					</div>
 					<div class="footer-bottom row">
 						<p class="footer-text m-0 col-lg-6 col-md-12">
@@ -162,15 +162,9 @@
 Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 						</p>
-						<div class="footer-social col-lg-6 col-md-12">
-							<a href="#"><i class="fa fa-facebook"></i></a>
-							<a href="#"><i class="fa fa-twitter"></i></a>
-							<a href="#"><i class="fa fa-dribbble"></i></a>
-							<a href="#"><i class="fa fa-behance"></i></a>
-						</div>
 					</div>
 				</div>
-			</footer>	
+			</footer>
 			<!-- End footer Area -->
 
 			<script src="{{asset('cbi_user/js/vendor/jquery-2.2.4.min.js')}}"></script>
