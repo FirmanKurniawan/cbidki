552a324a690931aa26b2e990b474b6067602024a
Andrea <Andrea>
2019-01-29 17:28:15 +0700

    Update

--- resources/views/admin/merchandise/edit.blade.php
+++ resources/views/admin/merchandise/edit.blade.php
@@ -8,7 +8,7 @@
                     <div class="row">
                         <div class="col-lg-3"></div>
                         <form action="{{url('admin/merchandise/update')}}" method="POST" id="adminpro-order-form" class="adminpro-form" enctype="multipart/form-data">
-                            <div class="col-lg-6">
+                            <div class="col-lg-12">
                                 <div class="login-bg">
                                     <div class="row">
                                         <div class="col-lg-12">
@@ -19,7 +19,7 @@
                                     
                                     <form>
                                       <div class="form-row">
-                                        <div class="form-group col-md-6">
+                                        <div class="form-group col-md-12">
                                           <label for="inputEmail4">Nama</label>
                                           <input type="hidden" name="id" value="{{$merchandise->id}}">
                                           <input type="text" class="form-control" id="inputEmail4" placeholder="Nama" name="nama" value="{{$merchandise->nama}}">
@@ -29,23 +29,31 @@
                                           <label for="inputCity">Deskripsi</label>
                                           <textarea id="ckeditor1" type="text" class="form-control" id="inputCity" name="deskripsi" placeholder="Deskripsi" value="">{!!$merchandise->deskripsi!!}</textarea>
                                         </div>
-                                      
-                                  </div>
-                                  <div class="form-row">
-                                      <div class="col-lg-8">
-                                        <div class="login-input-area">
-                                          <label>Logo</label>
-                                          <input name="logo" type="file" class="ts-forms" value="{{$merchandise->logo}}" >
-                                        </div>
+                                 
+                                        <br>
+                                        <center>
+                                          <div class="form-group col-md-4">
+                                          
+                                            <center>
+                                                <div class="container">        
+                                                  <img src="{{asset('picture1/noimg.png')}}" alt="Nature" class="responsive" id="blah1" style="width: 300px;height: 300px; margin-left: auto; margin-right: auto;">                                    
+                                                    <input name="logo" type="file" class="" style="" onchange="readURL1(this);" >
+                                                </div>
+
+                                            </center>
+                                          </div>
+
+
+                                  
+                                 
+                                      <br>
+                                      </div>
+                                        <div class="col-lg-8">
+                                          <div class="login-input-area">
+                                            <button type="submit" class="btn btn-primary" style="float: right; margin-top:280px;"><i class="fa fa-edit"></i></button>
                                       </div>
                                       </div>
                                       <br>
-                                      <div class="form-row">
-                                        <div class="col-lg-8">
-                                          <div class="login-input-area">
-                                            <button type="submit" class="btn btn-primary" style="margin-left: 430px;"><i class="fa fa-edit"></i></button>
-                                      </div>
-                                      </div>
                                       </div>
                                 </form>
                             @csrf
@@ -55,5 +63,19 @@
                         <div class="col-lg-3"></div>
                     </div>
                 </div>
+                 <script type="text/javascript">
+                         function readURL1(input) {
+                if (input.files && input.files[0]) {
+                    var reader = new FileReader();
+
+                    reader.onload = function (e) {
+                        $('#blah1')
+                            .attr('src', e.target.result);
+                    };
+
+                    reader.readAsDataURL(input.files[0]);
+                    }
+                }
+            </script>
             </div>
             @endsection
