0e4b708df7f3d4aa15ab490fdeb2f2015cd070c9
Firman Kurniawan <mrroz3r@gmail.com>
2019-01-30 20:57:05 +0700

    New Search

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -10,6 +10,11 @@ opacity:1;
 width:200px;
 height:200px;
 }
+.kiwil
+{
+  position: sticky;
+ margin-left: 30px;
+}
 .bulat2{
 border-radius:100em;
 opacity:1;
@@ -31,6 +36,11 @@ height:150px;
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
 <section class="service-page-area section-gap">
 <div class="container" style="margin-top: 5%;">
+  @if (session('alert'))
+  <div class="alert alert-success">
+      {{ session('alert') }}
+  </div>
+  @endif
     <center>
     <h1>Cari Anggota</h1>
     </center>   
@@ -50,7 +60,6 @@ height:150px;
 <br>
   
 <script type="text/javascript">
-
    $(document).ready(function() {
     $('#submit').click(function() {
         $.ajaxSetup({
@@ -72,8 +81,6 @@ height:150px;
         }); 
     });
 });  
-
-
 </script>
 
 <!-- Modal -->
@@ -96,13 +103,14 @@ height:150px;
          <div class="float-right" style="margin-left: 30px;">  
           <label>Nama : <input type="text" id="nama" readonly="true" class="form-control" style="background-color: #fff"></label>
       <br>
-      <label>Alamat :<input type="text" id="alamat" readonly="true" class="form-control"style="background-color: #fff" ></label>
+      <label>Alamat :<textarea type="text" id="alamat" readonly="true" class="form-control"style="background-color: #fff" >></textarea></label>
       <br>
       <label>Nomor Identitas<input type="number" id="no_identitas" readonly="true" class="form-control"style="background-color: #fff"></label> 
           <!-- {{-- <img src="{{url('images/'.$foto)}}"> --}} -->
 
 </div>
 </div>
+
       </div>
     </div>
   </div>
@@ -129,9 +137,9 @@ height:150px;
                                     $km = App\Korwilmember::where('idkorwil',$k->id)->get();
                                  ?>
                                  @foreach($km as $ka)
-                                <ol class="text-center" style="">
-                                  <li><h5 class="">&nbsp;&nbsp;&nbsp;&nbsp;{{$i++}}.&nbsp;<img src="{{url('images/'.$ka->logo)}}" style="width: 50px;">&nbsp;{{$ka->nama}}&nbsp;({{$ka->kode}})</h5></li>
- 
+                                <ol class="kiwil">
+                                  <li style="color: black;">
+                                    <br>  &nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-arrow-right"></i> &nbsp;<img src="{{url('images/'.$ka->logo)}}" style="width: 50px;">&nbsp;{{$ka->nama}}&nbsp;({{$ka->kode}})    </li>
                                     </ol>
                                     @endforeach
 <br>
